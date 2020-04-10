6d886e5991f8240ce72dbaf73126e99d83fcaf48
Jacques Vella Critien <jacques@cloudlabs.com.mt>
2020-04-10 17:00:44 +0200

    Adding test for shell code

---
+++ src/test/java/ShellCodeTest.java
@@ -0,0 +1,29 @@
+import jdk.nashorn.tools.Shell;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ShellCodeTest {
+
+    ShellCode shellCode;
+    @Before
+    public void setup()
+    {
+        shellCode = new ShellCode();
+    }
+
+    @After
+    public void teardown()
+    {
+        shellCode = null;
+    }
+
+    @Test
+    public void testGetHelloWorld()
+    {
+        String actual = shellCode.getHelloWorld();
+        assertEquals("Asserting getting hello world string", "Hello World!", actual);
+    }
+
+}
