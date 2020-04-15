1696c9d36ba3b4096767b41b0c52c73d995f2080
Jacques Vella Critien <jacques@cloudlabs.com.mt>
2020-04-15 16:44:05 +0200

    Added player package and Added test for player setter and getter

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import com.sun.media.jfxmedia.events.PlayerEvent;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import player.Player;
+import player.Position;
+
+import static org.junit.Assert.*;
 
 public class PlayerTest {
+
+    Player player;
+
+    @Before
+    public void setup() {
+        player = new Player();
+    }
+
+    @After
+    public void teardown() {
+        player = null;
+    }
+
+    /**
+     * Test for getter and setter
+     */
+    @Test
+    public void testPositionGetterAndSetter() {
+        Position position = new Position(1, 0);
+        player.setPosition(position);
+
+        //getting player actual coordinates
+        Position playerPos = player.getPosition();
+
+        //checking x coordinate from get
+        assertEquals("Asserting x position coordinate", 1, playerPos.getxCoordinate());
+        //checking y coordinate from get
+        assertEquals("Asserting  y coordinate", 0, playerPos.getyCoordinate());
+    }
 }
