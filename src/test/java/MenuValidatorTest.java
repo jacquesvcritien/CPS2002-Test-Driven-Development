1d4ef8d0cd590e4963d2e4a3f1a95618ddae11d3
joncut99 <jonathancutajar14@gmail.com>
2020-04-15 18:53:40 +0200

    Added test for amount of players

--- src/test/java/MenuValidatorTest.java
+++ src/test/java/MenuValidatorTest.java
@@ -1,3 +1,4 @@
+import menu.MenuValidator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -6,4 +7,33 @@ import static org.junit.Assert.*;
 
 
 public class MenuValidatorTest {
+
+    MenuValidator menuValidator;
+
+    /**
+     * Test for amount of players, one lower than range
+     */
+    @Test
+    public void lowerAmtPlayersValTest(){
+        boolean result = menuValidator.amtPlayersValidator(1);
+        assertFalse("Asserting a lower amount of players", result);
+    }
+
+    /**
+     * Test for amount of players, one higher than range
+     */
+    @Test
+    public void higherAmtPlayersValTest(){
+        boolean result = menuValidator.amtPlayersValidator(9);
+        assertFalse("Asserting a lower amount of players", result);
+    }
+
+    /**
+     * Test for amount of players with in the range for example 5
+     */
+    @Test
+    public void amtPlayersValTest(){
+        boolean result = menuValidator.amtPlayersValidator(5);
+        assertTrue("Asserting a lower amount of players", result);
+    }
 }
