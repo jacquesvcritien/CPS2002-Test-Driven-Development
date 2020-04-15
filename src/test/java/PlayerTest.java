3490256673ce70bd247f200e638b74116436188a
Jacques Vella Critien <jacques@cloudlabs.com.mt>
2020-04-15 17:26:29 +0200

    Added test for move

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -2,6 +2,7 @@ import com.sun.media.jfxmedia.events.PlayerEvent;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import player.Direction;
 import player.Player;
 import player.Position;
 
@@ -36,5 +37,32 @@ public class PlayerTest {
         assertEquals("Asserting x position coordinate", 1, playerPos.getxCoordinate());
         //checking y coordinate from get
         assertEquals("Asserting  y coordinate", 0, playerPos.getyCoordinate());
+    }
+
+    /**
+     * Test for getter and setter
+     */
+    @Test
+    public void testMove() {
+        Position position = new Position(0, 0);
+        player.setPosition(position);
+
+        //move up
+        player.move(Direction.UP);
+        //checking x coordinate after move up
+        assertEquals("Asserting y position after move up", 1, player.getPosition().getyCoordinate());
+        //move down
+        player.move(Direction.DOWN);
+        //checking x coordinate after move down
+        assertEquals("Asserting y position after move down", 0, player.getPosition().getyCoordinate());
+        //move right
+        player.move(Direction.RIGHT);
+        //checking x coordinate after move right
+        assertEquals("Asserting x position after move right", 1, player.getPosition().getxCoordinate());
+        //move left
+        player.move(Direction.LEFT);
+        //checking x coordinate after move left
+        assertEquals("Asserting y position after move left", 0, player.getPosition().getxCoordinate());
+
     }
 }
