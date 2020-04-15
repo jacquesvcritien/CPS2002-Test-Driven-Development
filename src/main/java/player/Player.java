7e6592cb1c675d90459f8753047345dc87011491
Jacques Vella Critien <jacques@cloudlabs.com.mt>
2020-04-15 16:44:57 +0200

    Added getters and setters for player

--- src/main/java/player/Player.java
+++ src/main/java/player/Player.java
@@ -1,4 +1,31 @@
 package player;
 
 public class Player {
+    //stores position for player
+    private Position position;
+
+    /**
+     *  Method to move player
+     * @param direction direction to move
+     */
+    public void move(Direction direction)
+    {
+
+    }
+
+    /**
+     * Getter for position
+     * @return position of player
+     */
+    public Position getPosition() {
+        return position;
+    }
+
+    /**
+     * Setter for position
+     * @param position psoition to set
+     */
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
