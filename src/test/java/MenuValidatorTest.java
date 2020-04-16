624e8717ccc82a2bae757a3f57ed9523e45d056d
joncut99 <jonathancutajar14@gmail.com>
2020-04-16 10:52:56 +0200

    Tests for map sizes

--- src/test/java/MenuValidatorTest.java
+++ src/test/java/MenuValidatorTest.java
@@ -25,7 +25,7 @@ public class MenuValidatorTest {
     @Test
     public void higherAmtPlayersValTest(){
         boolean result = menuValidator.amtPlayersValidator(9);
-        assertFalse("Asserting a lower amount of players", result);
+        assertFalse("Asserting a higher amount of players", result);
     }
 
     /**
@@ -34,6 +34,60 @@ public class MenuValidatorTest {
     @Test
     public void amtPlayersValTest(){
         boolean result = menuValidator.amtPlayersValidator(5);
-        assertTrue("Asserting a lower amount of players", result);
+        assertTrue("Asserting an amount of players in the range", result);
+    }
+
+    /**
+     * Test a smaller value than map size required for players 2-4
+     */
+    @Test
+    public void lowerSmallMapSizeTest(){
+        boolean result = menuValidator.smallMapSize(4);
+        assertFalse("Asserting a lower map size", result);
+    }
+
+    /**
+     * Test a larger value than map size required for players 2-4
+     */
+    @Test
+    public void higherSmallMapSizeTest(){
+        boolean result = menuValidator.smallMapSize(51);
+        assertFalse("Asserting a larger map size", result);
+    }
+
+    /**
+     * Test map size value in range for players 2-4
+     */
+    @Test
+    public void smallMapSizeTest(){
+        boolean result = menuValidator.smallMapSize(20);
+        assertTrue("Asserting a map size in range as expected", result);
+    }
+
+    /**
+     * Test a smaller value than map size required for players 5-8
+     */
+    @Test
+    public void lowerLargeMapSizeTest(){
+        boolean result = menuValidator.bigMapSize(7);
+        assertFalse("Asserting a lower map size", result);
+    }
+
+    /**
+     * Test a larger value than map size required for players 5-8
+     */
+    @Test
+    public void higherLargeMapSizeTest(){
+        boolean result = menuValidator.bigMapSize(51);
+        assertFalse("Asserting a larger map size", result);
+    }
+
+    /**
+     * Test map size value in range for players 5-8
+     */
+    @Test
+    public void bigMapSizeTest(){
+        boolean result = menuValidator.bigMapSize(25);
+        assertTrue("Asserting a map size in range as expected", result);
     }
 }
