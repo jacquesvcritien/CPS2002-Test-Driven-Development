3395c55821453e0395118159cee4353b9791a48d
Jacques Vella Critien <jacques@cloudlabs.com.mt>
2020-04-16 18:01:10 +0200

    Added tests for tiles

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,96 @@
+import map.BlueTile;
+import map.GreenTile;
+import map.TileType;
+import map.TreasureTile;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import player.Position;
+
+import static org.junit.Assert.*;
+
+public class TileTest {
+
+    GreenTile greenTile;
+    BlueTile blueTile;
+    TreasureTile treasureTile;
+    @Before
+    public void setup()
+    {
+        greenTile = new GreenTile();
+        blueTile = new BlueTile();
+        treasureTile = new TreasureTile();
+    }
+
+    @After
+    public void teardown()
+    {
+        greenTile = null;
+        blueTile = null;
+        treasureTile = null;
+    }
+
+    /**
+     * Test for green tile type getter
+     */
+    @Test
+    public void testGetGreenType()
+    {
+        //test for green tile
+        assertEquals("Asserting green tile type", TileType.GREEN, greenTile.getType());
+    }
+
+    /**
+     * Test for blue tile type getter
+     */
+    @Test
+    public void testGetBlueType()
+    {
+        //test for blue tile
+        assertEquals("Asserting blue tile type", TileType.BLUE, blueTile.getType());
+    }
+
+    /**
+     * Test for treasure tile type getter
+     */
+    @Test
+    public void testGetTreasureType()
+    {
+        //test for treasure tile
+        assertEquals("Asserting treasure tile type", TileType.TREASURE, treasureTile.getType());
+    }
+
+    /**
+     * Test for green tile html getter
+     */
+    @Test
+    public void testGreenGetHtml()
+    {
+        //test for green tile
+        assertEquals("Asserting green tile html", "<td class=\"grass-back\"></td>", greenTile.getHtml());
+    }
+
+    /**
+     * Test for blue tile html getter
+     */
+    @Test
+    public void testBlueGetHtml()
+    {
+        //test for blue tile
+        assertEquals("Asserting blue tile html", "<td class=\"water-back\"></td>", blueTile.getHtml());
+    }
+
+    /**
+     * Test for treasure tile html getter
+     */
+    @Test
+    public void testTreasureGetHtml()
+    {
+        //test for treasure tile
+        assertEquals("Asserting treasure tile html", "<td class=\"grass-back\"><img class=\"tile-img\" src=\"images/treasure.png\"></td>", treasureTile.getHtml());
+    }
+
+
+
+
+}
