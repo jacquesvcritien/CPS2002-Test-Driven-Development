86bba074c9cb418931ae6105b84c4fe4054c322e
joncut99 <jonathancutajar14@gmail.com>
2020-04-16 16:33:53 +0200

    Added method for map size

--- src/main/java/menu/MenuValidator.java
+++ src/main/java/menu/MenuValidator.java
@@ -4,10 +4,38 @@ public class MenuValidator {
 
     /**
      * Method to check the amount of players between 2 & 8
-     * @param players
+     * @param players amount of players
      * @return
      */
     public static boolean amtPlayersValidator(int players){
         return players >= 2 && players <= 8;
     }
+
+    /**
+     * Method to check map size is set accordingly
+     * @param players amount of players
+     * @param size map size
+     * @return
+     */
+    public static boolean mapSize(int players, int size){
+        if(players >= 2 && players <= 4){
+            if(size >= 5 && size <= 50){
+                return true;
+            }
+            else{
+                System.out.println("Map size should between between 5-50");
+                return false;
+            }
+        }
+        else{
+            if(size >= 8 && size <= 50){
+                return true;
+            }
+            else{
+                System.out.println("Map size should between between 8-50");
+                return false;
+            }
+        }
+    }
+
 }
