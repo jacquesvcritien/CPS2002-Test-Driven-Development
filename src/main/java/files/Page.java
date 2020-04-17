c2ccb17b22fcd6cc9ca9fb52ac955fabc1ec9d5d
Jacques Vella Critien <jacques.vella-critien.18@um.edu.mt>
2020-04-17 18:03:35 +0200

    Added class for page

---
+++ src/main/java/files/Page.java
@@ -0,0 +1,15 @@
+package files;
+
+public class Page {
+
+    String HTML;
+
+    /**
+     * Method to return html
+     * @return returns html
+     */
+    public String getHTML()
+    {
+        return HTML;
+    };
+}
