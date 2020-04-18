590d63b79a5e818b98fd8856bc2e93200ea98393
joncut99 <jonathancutajar14@gmail.com>
2020-04-19 01:52:57 +0200

    Helper class used for menu

---
+++ src/main/java/menu/Helper.java
@@ -0,0 +1,34 @@
+package menu;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Helper {
+    /**
+     * A validation that the input is integer
+     * @param scanner
+     * @param message
+     * @param notANumberError message if the input is not an integer
+     * @return
+     */
+    public static int integerVal(Scanner scanner, String message, String notANumberError)
+    {
+        int intInput = 0;
+
+        do {
+            System.out.println(message);
+
+            try {
+                intInput = scanner.nextInt();
+
+                if(intInput == 0)
+                    break;
+            }catch(InputMismatchException e) {
+                System.out.println(notANumberError);
+                scanner.next();
+            }
+        }while(intInput == 0);
+
+        return intInput;
+    }
+}
