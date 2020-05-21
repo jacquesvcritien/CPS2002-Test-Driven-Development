25e282598de62a987d200a9f5117513e3bb4ce3f
joncut99 <jonathancutajar14@gmail.com>
2020-05-21 21:49:00 +0200

    Added tests for notifyallplayers method

--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -6,10 +6,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.verification.VerificationMode;
 import team.Team;
 import team.player.Direction;
 import team.player.Player;
@@ -50,7 +47,55 @@ public class TeamTest {
     {
         team = null;
     }
-    
+
+    /**
+     * Test for notifyAllPlayers
+     */
+    @Test
+    public void testNotifyAllPlayers() throws MapNotSetException, IOException, URISyntaxException {
+
+        Player[] players = new Player[3];
+        players[0] =  Mockito.mock(Player.class);
+        players[1] =  Mockito.mock(Player.class);
+        players[2] =  Mockito.mock(Player.class);
+
+        for(Player player: players)
+        {
+            Mockito.doNothing().when(player).update();
+            team.attach(player);
+        }
+
+        team.notifyAllPlayers();
+
+        Mockito.verify(players[0], Mockito.times(1)).update();
+        Mockito.verify(players[1], Mockito.times(1)).update();
+        Mockito.verify(players[2], Mockito.times(1)).update();
+
+    }
+
+    /**
+     * Test for notifyAllPlayers by checking the actual getting of state
+     */
+    @Test
+    public void testNotifyAllPlayersActualAction() throws MapNotSetException, IOException, URISyntaxException {
+
+        int amOfPlayers = 3;
+        Player[] players = new Player[amOfPlayers];
+
+        for(int i=0; i < amOfPlayers; i++)
+        {
+            players[i] = new Player();
+            Game.setPlayerToTeam(team, players[i]);
+        }
+
+        //move for team
+        team.setState(Direction.UP);
+
+        //assert all have UP as a move
+        for (Player player: players)
+            Assert.assertTrue("Asserting player has an UP move", player.getMoves().contains(Direction.UP));
+
+    }
 
 
 
